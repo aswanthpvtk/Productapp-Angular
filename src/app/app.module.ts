77f6b316dfdd16a95e2d4bc3d3f6f0dfd5966da3
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-06 21:21:32 +0530

    pro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,32 @@ import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { FormsModule } from '@angular/forms';
 import { ViewProductComponent } from './view-product/view-product.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const myroute:Routes =[
+
+  {
+    path :"",
+    component :AddProductComponent
+  },
+  {
+    path :"v",
+    component: ViewProductComponent
+  },
+]
 @NgModule({
   declarations: [
     AppComponent,
     AddProductComponent,
-    ViewProductComponent
+    ViewProductComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(myroute),
     FormsModule
   ],
   providers: [],
